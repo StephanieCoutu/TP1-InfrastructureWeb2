5dac8ef1d4a6e19700c10c72e047f033ffef2525
Stéphanie Coutu <steph_9_4@hotmail.com>
2023-06-03 22:38:44 -0400

    Formulaire complété !

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,81 +1,93 @@
-/* const form = document.getElementById('form');
-const textprenom = document.getElementById('textprenom');
-const textnom = document.getElementById('textnom');
-const email = document.getElementById('email');
-const message = document.getElementById('message');
-
-const validateForm = () => {
-    let noError = true;
-    const textprenom = textprenom.value.trim();
-    const textnom = textnom.value.trim();
-    const email = email.value.trim();
-    const message = message.value.trim();
-
-    if (textprenomValue === '') {
-        setError(textprenom, 'Votre prénom est requis');
-        noError = false;
-    }else if (textnom === '') {
-        setError(textnom, 'Votre nom est requis');
-        noError = false;
-    }else if (email === '') {
-        setError(email, 'Votre adresse courriel est requis');
-        noError = false;
-    }else if (message === '') {
-        setError(message, 'Une briève description de votre contact est requise');
-        noError = false;
-    }else {
-        return noError;
-    }
-} */
-
-const form = document.querySelector('form');
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  validateForm();
-});
-
 function validateForm() {
-    const inputNom = document.getElementById('textname');
-    const inputPrenom = document.getElementById('textrenom');
-    const inputEmail = document.getElementById('email');
-    const inputMessage = document.getElementById('message');
-  
-    const nom = inputNom.value;
-    const prenom = inputPrenom.value;
-    const email = inputEmail.value;
-    const message = inputMessage.value;
-  
-    if (nom === '') {
-      inputNom.classList.add('error');
-      document.getElementById('nom-error').textContent = 'Ce champs est requis';
-    } else {
+
+  try {
+
+      const inputPrenom = document.getElementById('textprenom');
+      const inputNom = document.getElementById('textnom');
+
+      const inputEmail = document.getElementById('email');
+      const inputMessage = document.getElementById('message');
+
+      const inputStatus = document.getElementById('status');
+
+      const prenom = inputPrenom.value;
+      const nom = inputNom.value;
+      const email = inputEmail.value;
+      const message = inputMessage.value;
+
+      inputPrenom.classList.remove('error');
       inputNom.classList.remove('error');
-      document.getElementById('nom-error').textContent = '';
-    }
+      inputEmail.classList.remove('error');
+      inputMessage.classList.remove('error');
 
-    if (prenom === '') {
-        inputPrenom.classList.add('error');
-        document.getElementById('prenom-error').textContent = 'Ce champs est requis';
+      document.getElementById('textprenom-error').classList.remove('error-star');
+      document.getElementById('textnom-error').classList.remove('error-star');
+      document.getElementById('email-error').classList.remove('error-star');
+      document.getElementById('message-error').classList.remove('error-star');
+
+      inputStatus.innerHTML = "* Ce champ est requis!";
+
+      if (prenom != '') {
+
+          if (nom != '') {
+
+            if (email != '') {
+
+              if (message != '') {
+
+                  inputStatus.innerHTML = "Redirection en cours...";
+
+                  return true;
+
+
+              } else {
+
+                  inputMessage.classList.add('error');
+
+                  document.getElementById('message-error').classList.add('error-star');
+
+                  return false;
+
+              }
+
+          } else {
+
+              inputEmail.classList.add('error');
+
+              document.getElementById('email-error').classList.add('error-star');
+
+              return false;
+
+          }
+
+        } else {
+
+            inputNom.classList.add('error');
+
+            document.getElementById('textnom-error').classList.add('error-star');
+
+            return false;
+
+        }
+
       } else {
-        inputPrenom.classList.remove('error');
-        document.getElementById('prenom-error').textContent = '';
+
+          inputPrenom.classList.add('error');
+
+          document.getElementById('textprenom-error').classList.add('error-star');
+
+          return false;
+
       }
 
-      if (email === '') {
-        inputEmail.classList.add('error');
-        document.getElementById('email-error').textContent = 'Ce champs est requis';
-      } else {
-        inputEmail.classList.remove('error');
-        document.getElementById('email-error').textContent = '';
-      }
+  } catch (e) {
 
-      if (message === '') {
-        inputMessage.classList.add('error');
-        document.getElementById('message-error').textContent = 'Ce champs est requis';
-      } else {
-        inputMessage.classList.remove('error');
-        document.getElementById('message-error').textContent = '';
-      }
-  };
+      alert("Une exception inattendue s'est produite. Veuillez rejoindre l'administrateur de service!");
 
+      return false;
+
+  }
+  
+};
+
+ 
